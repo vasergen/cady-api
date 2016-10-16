f8b232975a3d103de08ce9cf9d686f445ecb8dab
vasergen <vasergen@gmail.com>
2016-10-16 04:05:39 +0200

    chore(looger): add logger on every responce

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@
 const routes = require('./routes')
 const dbConnect = require('./lib/dbConnect')
 const serverStart = require('./lib/serverStart')
+const logger = require('./lib/logger')
 
 const db = dbConnect()
 const server = serverStart()
 
 server.route(routes)
 
+server.on('response', function (request) {
+    const message =
+        request.info.remoteAddress + ': ' + request.method.toUpperCase()
+        + ' ' + request.url.path + ' --> ' + request.response.statusCode
+
+    logger.info(message)
+});
 
--- server/lib/pmongoose.js
+++ server/lib/pmongoose.js
@@ -2,6 +2,14 @@
 
 const mongoose = require('mongoose')
 const Promise = require('bluebird')
+const config = require('config')
+
 Promise.promisifyAll(mongoose)
+
+const logLevel = config.get('logLevel')
+
+if(['silly', 'debug'].indexOf(logLevel) !== -1) {
+    mongoose.set('debug', true)
+}
 
 module.exports = mongoose
