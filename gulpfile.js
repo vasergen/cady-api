12b855ac13370fd8019c67bc6c639b14c1bbe89a
vasergen <vasergen@gmail.com>
2016-10-18 21:01:57 +0200

    feat(test_coverage): add istanbul test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
 
 const gulp = require('gulp')
 const mocha = require('gulp-mocha')
+const istanbul = require('gulp-istanbul')
 
-gulp.task('test', () => {
+gulp.task('pre-test', function () {
+    return gulp.src(['server/**/*.js', '!server/**/*.spec.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], () => {
     return gulp.src('server/**/*.spec.js')
         .pipe(mocha({}))
+        .pipe(istanbul.writeReports())
 })
 
 gulp.task('test:watch' ,() => {
