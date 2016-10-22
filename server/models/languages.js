7f0fdb38870e84c8e242009dbfed2eda5095083a
vasergen <vasergen@gmail.com>
2016-10-22 14:00:10 +0200

    chore(LanguageListModel): Rename LanguageModel to LanguageModelList

--- server/models/languages.js
+++ server/models/languages.js
@@ -1,22 +1,27 @@
 'use strict'
 
+/**
+ * This model is only list of languages
+ * @type {mongoose|exports|module.exports}
+ */
+
 const mongoose = require('../lib/pmongoose')
 const BaseSchema = require('./baseSchema')
 const Joi = require('joi')
 
 //Mongo Schema
-const LanguageSchema = BaseSchema({
+const LanguageListSchema = BaseSchema({
     name: {type: String, trim: true, required: true},
     slug: {type: String, trim: true},
     language: {type: String, trim: true, required: true}
 })
 
-LanguageSchema.statics.joiValidate = {
+LanguageListSchema.statics.joiValidate = {
     name: Joi.string().alphanum().min(1).max(100).required(),
     slug: Joi.string(),
     language: Joi.string().alphanum().min(1).required()
 }
 
-const LanguageModel = mongoose.model('Language', LanguageSchema)
+const LanguageListModel = mongoose.model('LanguageList', LanguageListSchema)
 
-module.exports = LanguageModel
+module.exports = LanguageListModel
